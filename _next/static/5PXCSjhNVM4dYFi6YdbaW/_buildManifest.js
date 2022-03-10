6e8667861ef4ac8e7923d224a3a5ae8b0b939753
HitsujiRere <HitsujiRere@users.noreply.github.com>
2022-03-10 04:09:40 +0000

    deploy: 4cbda6c47fbdde40455084f4329091d8a397825c

--- _next/static/5PXCSjhNVM4dYFi6YdbaW/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-af17c1c2a1d18bd3.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
