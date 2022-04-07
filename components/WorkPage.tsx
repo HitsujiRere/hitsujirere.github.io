829390fd0db25ae646f99661c5acea4f3058b356
HitsujiRere <hitsujirere@gmail.com>
2022-04-07 22:33:12 +0900

    fix

--- components/WorkPage.tsx
+++ components/WorkPage.tsx
@@ -51,7 +51,7 @@ const WorkPage = (props: {
                     <Link href={props.linkWork}>
                       <a className='flex justify-start md:justify-end'>
                         <p className='text-xl'>Work Website</p>
-                        <svg className="h-8 w-8"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <path d="M11 7h-5a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-5" />  <line x1="10" y1="14" x2="20" y2="4" />  <polyline points="15 4 20 4 20 9" /></svg>
+                        <svg className="h-8 w-8"  width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <path d="M11 7h-5a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-5" />  <line x1="10" y1="14" x2="20" y2="4" />  <polyline points="15 4 20 4 20 9" /></svg>
                       </a>
                     </Link>
                   </div>
