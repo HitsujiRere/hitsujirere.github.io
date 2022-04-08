3a25568e8eeaf0023508ea13a563a36d2a952044
HitsujiRere <hitsujirere@gmail.com>
2022-04-08 20:55:58 +0900

    lint codes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,17 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Header from '../components/Header'
-import Start from '../components/Start'
-import About from '../components/About'
-import Profile from '../components/Profile'
-import Works from '../components/Works'
-import Footer from '../components/Footer'
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Header from '../components/Header';
+import Start from '../components/Start';
+import About from '../components/About';
+import Profile from '../components/Profile';
+import Works from '../components/Works';
+import Footer from '../components/Footer';
 
 const Home: NextPage = () => {
   return (
     <div>
       <Head>
-        <title>Rere{"'"}s Portfolio</title>
+        <title>Rere{'\''}s Portfolio</title>
         <meta name="description" content="Rere's Portfolio" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
@@ -30,7 +30,7 @@ const Home: NextPage = () => {
 
       <Footer />
     </div>
-  )
-}
+  );
+};
 
 export default Home;
