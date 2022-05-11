3c108291d34ec0204a490221b9ebf89490d329dd
HitsujiRere <hitsujirere@gmail.com>
2022-05-11 19:03:46 +0900

    Profileのコードカラーを変更

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -1,7 +1,5 @@
 import SyntaxHighlighter from 'react-syntax-highlighter';
-import { vs2015 } from 'react-syntax-highlighter/dist/cjs/styles/hljs';
-
-vs2015['hljs']['background'] = 'transparent';
+import { style } from './SyntaxHighlightStyle';
 
 const calcAge = (birthdate: Date, today: Date) => {
   const birthday = new Date(today.getFullYear(), birthdate.getMonth(), birthdate.getDate());
@@ -22,16 +20,17 @@ class DejimaMikihide {
   languages = {
     good: [ 'C', 'C++', 'C#', 'JavaScript', 'Python', ],
     hadUsed: [ 'Java', 'TypeScript', ],
-    learning: [ 'Rust', 'TypeScript', 'Go', ],
+    learning: [ 'Rust', 'TypeScript', 'Go', ]
   };
   frameworks = {
     good: [ 'Node.js', 'Processing', '.NET Framework', ],
-    hadUsed: [ 'Next.js', ],
+    hadUsed: [ 'React', 'Next.js', ],
+    learning: [ 'React', 'Next.js', ]
   };
   skills = {
     competitiveProgramming: {
       language: [ 'C++', ],
-      AtCoder: { user: 'Deji', highestRate: 1455, }
+      AtCoder: { user: 'Deji', highestRate: 1455 }
     }
   };
 }
@@ -42,8 +41,8 @@ const Profile = () => {
     <div id="profile" className="px-4 py-20 bg-gray-900 md:hover:bg-gray-800 duration-500">
       <div className="mx-auto max-w-screen-lg space-y-5">
         <h2 className="text-2xl font-light text-yellow-400">PROFILE</h2>
-        <div className="flex justify-center tracking-wider">
-          <SyntaxHighlighter language="javascript" style={vs2015}>
+        <div className="flex justify-center text-lg tracking-wider">
+          <SyntaxHighlighter language="javascript" style={style} showLineNumbers>
             {profile}
           </SyntaxHighlighter>
         </div>
