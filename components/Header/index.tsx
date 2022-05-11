4c30d0ff5d819d74f0ebaee27b0641dec6500298
HitsujiRere <hitsujirere@gmail.com>
2022-05-11 20:49:53 +0900

    componentsを整理

---
+++ components/Header/index.tsx
@@ -0,0 +1,29 @@
+import HeaderLink from './HeaderLink';
+
+const Header = () => {
+  return (
+    <>
+      <div id="scroll-scapegoat" className="fixed"></div>
+
+      <header className="w-screen p-8 bg-gray-900 md:bg-transparent md:fixed md:flex md:justify-between md:items-center md:z-10">
+        <HeaderLink to="start">
+          <h1 className="text-xl font-bold text-white">Rere{'\''}s Portfolio</h1>
+        </HeaderLink>
+
+        <nav className="hidden md:flex md:gap-10">
+          <HeaderLink to="about">
+            <p className="text-xl font-bold text-gray-200">ABOUT</p>
+          </HeaderLink>
+          <HeaderLink to="profile">
+            <p className="text-xl font-bold text-gray-200">PROFILE</p>
+          </HeaderLink>
+          <HeaderLink to="works">
+            <p className="text-xl font-bold text-gray-200">WORKS</p>
+          </HeaderLink>
+        </nav>
+      </header>
+    </>
+  );
+};
+
+export default Header;
--- components/Works/index.tsx
+++ components/Works/index.tsx
@@ -1,51 +1,5 @@
-import Link from 'next/link';
-import { ReactNode, useState } from 'react';
-
-const WorkBoxImage = (props: { href?: string, image: string, size: string, position: string }) => {
-  return (
-    props.href ? (
-      <Link href={props.href}>
-        <a className="h-60">
-          <img
-            src={props.image}
-            className={`h-full w-full rounded-xl ${props.size} ${props.position} ${props.size === 'object-contain' && 'bg-gray-100'}`}
-            alt="cover image"
-            loading="lazy"
-          />
-        </a>
-      </Link>
-    ) : (
-      <div className="h-60">
-        <img
-          src={props.image}
-          className={`h-full w-full rounded-xl ${props.size} ${props.position} ${props.size === 'object-contain' && 'bg-gray-100'}`}
-          alt="cover image"
-          loading="lazy"
-        />
-      </div>
-    )
-  );
-};
-
-const WorkBoxText = (props: { href?: string, children?: ReactNode }) => {
-  return (
-    props.href ? (
-      <Link href={props.href}>
-        <a className="h-60 p-4 rounded-xl bg-gray-100 flex items-center">
-          <div className="w-full text-center space-y-5 select-none">
-            {props.children}
-          </div>
-        </a>
-      </Link>
-    ) : (
-      <div className="h-60 p-4 rounded-xl bg-gray-100 flex items-center">
-        <div className="w-full text-center space-y-5 select-none">
-          {props.children}
-        </div>
-      </div>
-    )
-  );
-};
+import { useState } from 'react';
+import { WorkBoxImage, WorkBoxText } from './WorkBox';
 
 const Works = () => {
   const [isDetailsOpened, setDetailsOpened] = useState(false);
