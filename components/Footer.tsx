24df286d43ff85f4cd04155cf5e1227bf4263d55
HitsujiRere <hitsujirere@gmail.com>
2022-07-29 23:04:23 +0900

    wip

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <header className='p-8 text-center bg-gray-900'>
-      <p className='text-xs text-gray-200'>DEJIMA MIKIHIDE - 2022</p>
+      <p className='text-xs text-gray-200'>HITSUJI RERE - 2022</p>
     </header>
   );
 };
--- components/Start/index.tsx
+++ components/Start/index.tsx
@@ -10,7 +10,7 @@ const Start = () => {
           <h2 className='text-2xl font-light text-yellow-400'>Let{"'"}s go!</h2>
           <p className='text-5xl font-bold text-white'>What will we make with a computer?</p>
           <div>
-            <p className='text-xl font-bold text-gray-400'>Hello! I{"'"}m Dejima Mikihide.</p>
+            <p className='text-xl font-bold text-gray-400'>Hello!</p>
             <p className='text-xl font-bold text-gray-400'>
               I{"'"}m a student studying to become a computer programmer.
             </p>
