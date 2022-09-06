ec3cd3d6f59fd45e122a76aad841f039ab5f6ddd
HitsujiRere <hitsujirere@gmail.com>
2022-09-07 00:06:40 +0900

    wip

--- components/pages/Header.tsx
+++ components/pages/Header.tsx
@@ -2,22 +2,22 @@ import { HeaderLink } from '@/components/molecules/HeaderLink';
 
 export const Header = () => {
   return (
-      <header className='w-screen p-8 bg-gray-900 md:bg-transparent md:fixed md:flex md:justify-between md:items-center md:z-10'>
-        <HeaderLink to='start'>
-          <h1 className='text-xl font-bold text-white'>Rere{"'"}s Portfolio</h1>
-        </HeaderLink>
+    <header className='w-screen p-8 bg-gray-900 md:bg-transparent md:fixed md:flex md:justify-between md:items-center md:z-10'>
+      <HeaderLink to='start'>
+        <h1 className='text-xl font-bold text-white'>Rere{"'"}s Portfolio</h1>
+      </HeaderLink>
 
-        <nav className='hidden md:flex md:gap-10'>
-          <HeaderLink to='about'>
-            <p className='text-xl font-bold text-gray-200'>ABOUT</p>
-          </HeaderLink>
-          <HeaderLink to='profile'>
-            <p className='text-xl font-bold text-gray-200'>PROFILE</p>
-          </HeaderLink>
-          <HeaderLink to='works'>
-            <p className='text-xl font-bold text-gray-200'>WORKS</p>
-          </HeaderLink>
-        </nav>
-      </header>
+      <nav className='hidden md:flex md:gap-10'>
+        <HeaderLink to='about'>
+          <p className='text-xl font-bold text-gray-200'>ABOUT</p>
+        </HeaderLink>
+        <HeaderLink to='profile'>
+          <p className='text-xl font-bold text-gray-200'>PROFILE</p>
+        </HeaderLink>
+        <HeaderLink to='works'>
+          <p className='text-xl font-bold text-gray-200'>WORKS</p>
+        </HeaderLink>
+      </nav>
+    </header>
   );
 };
