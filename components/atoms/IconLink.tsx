ebebaa0574d0e1bff9521e1d7e0d876b0496cafe
HitsujiRere <hitsujirere@gmail.com>
2022-10-28 20:15:34 +0900

    Linkの修正

--- components/atoms/IconLink.tsx
+++ components/atoms/IconLink.tsx
@@ -2,29 +2,25 @@ import Link from 'next/link';
 import { IconContext } from 'react-icons';
 import { RiGithubLine, RiTwitterLine, RiExternalLinkLine } from 'react-icons/ri';
 
-export const IconLink = (
-  props: {
-    icon: 'github' | 'twitter' | 'external';
-    to: string;
-    text: string;
-  } & JSX.IntrinsicElements['a'],
-) => {
-  const { icon, to, text, ...aProps } = props;
+export const IconLink = (props: {
+  icon: 'github' | 'twitter' | 'external';
+  to: string;
+  text: string;
+}) => {
+  const { icon, to, text } = props;
 
   return (
-    <Link href={to}>
-      <a
-        {...aProps}
-        className={`flex text-green-500 duration-500 hover:text-green-300 ${aProps.className}`}
-        target='__blank'
-      >
-        <IconContext.Provider value={{ size: '2rem' }}>
-          {icon === 'github' && <RiGithubLine />}
-          {icon === 'twitter' && <RiTwitterLine />}
-          {icon === 'external' && <RiExternalLinkLine />}
-        </IconContext.Provider>
-        <p className='text-xl'>{text}</p>
-      </a>
+    <Link
+      href={to}
+      className={`flex text-green-500 duration-500 hover:text-green-300`}
+      target='__blank'
+    >
+      <IconContext.Provider value={{ size: '2rem' }}>
+        {icon === 'github' && <RiGithubLine />}
+        {icon === 'twitter' && <RiTwitterLine />}
+        {icon === 'external' && <RiExternalLinkLine />}
+      </IconContext.Provider>
+      <p className='text-xl'>{text}</p>
     </Link>
   );
 };
