8ff2974a7f7104cca240be16b2d832e13e844db4
HitsujiRere <hitsujirere@gmail.com>
2022-12-17 16:49:02 +0900

    Worksのデザイン変更

--- src/components/molecules/WorkBox.tsx
+++ src/components/molecules/WorkBox.tsx
@@ -13,10 +13,11 @@ export const WorkBoxImage = (props: {
       <img
         src={props.image}
         className={classNames(
-          'h-60 w-full duration-500 hover:scale-110',
+          'aspect-[4/3] w-full',
           props.size,
           props.position,
           { 'bg-gray-100': props.size === 'object-contain' },
+          { 'duration-500 hover:scale-110': props.href },
         )}
         alt='cover image'
         loading='lazy'
@@ -30,7 +31,11 @@ export const WorkBoxImage = (props: {
 export const WorkBoxText = (props: { href?: string; children?: ReactNode }) => {
   const inner = (
     <div className='overflow-hidden rounded-xl'>
-      <div className='flex h-60 items-center bg-gray-100 p-4 duration-500 hover:scale-110'>
+      <div
+        className={classNames('flex h-60 items-center bg-gray-100 p-4', {
+          'duration-500 hover:scale-110': props.href,
+        })}
+      >
         <div className='w-full select-none space-y-5 text-center'>{props.children}</div>
       </div>
     </div>
